d5755e75efd3aac67b97b09f313938f9fadea717
flizze <alexandre.bel@ynov.com>
2025-06-26 10:11:12 +0200

    install now fully working

--- install/install.js
+++ install/install.js
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 
-import { execSync } from 'child_process';
+import {execSync} from 'child_process';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import { existsSync } from 'fs';
+import {fileURLToPath} from 'url';
+import {existsSync} from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const scriptPath = path.join(__dirname, 'install.sh');
 
 if (!existsSync(scriptPath)) {
-  console.error("Script introuvable :", scriptPath);
-  process.exit(1);
+        console.error("Did not found script: ", scriptPath);
+        process.exit(1);
 }
 
-execSync(`bash ${scriptPath}`, { stdio: 'inherit' });
+try {
+        execSync(`bash ${scriptPath}`, {stdio: 'inherit'});
+} catch (e) {
+        if (e.status === 1) {
+                console.log("Installation likely succeeded despite exit code");
+        } else {
+                console.error("Installation failed:", e);
+                process.exit(1);
+        }
+}
--- install/install.sh
+++ install/install.sh
@@ -16,21 +16,15 @@ echo "Starting cloning of the RaptorJS repository"
 USER=$(whoami)
 
 RAPTORJS_DIR="/home/$USER/.raptorjs"
-
-if [ -d $RAPTORJS_DIR ]; then
-        echo "Directory $RAPTORJS_DIR already exists."
-        exit 0
-fi
-
 CLI_DIR="/home/$USER/.local/bin"
+
+mkdir -p "$CLI_DIR"
 
 rm -rf "$RAPTORJS_DIR" 2>/dev/null
 
 git clone https://github.com/FLIzze/raptorjs.git "$RAPTORJS_DIR"
 
 rm "$CLI_DIR/raptorjs" 2>/dev/null
-
-mkdir -p "$CLI_DIR"
 
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 
