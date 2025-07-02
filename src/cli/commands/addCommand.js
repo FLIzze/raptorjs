2c4067b4dfcb7c7014c7f69c5c9f69f5eccec8a4
Erwan SINCK <erwan.sinck12@gmail.com>
2025-07-02 15:49:20 +0200

    test

--- src/cli/commands/addCommand.js
+++ src/cli/commands/addCommand.js
@@ -1,3 +1,20 @@
 export const addCommandFunc = async () => {
-    console.log("coucou")
+    const commandName = "test"
+    const description = "test description"
+    
+    const code = `\
+export const PingCommand = {
+
+    name:"${commandName}",
+    description:"${description}",
+    options:[],
+
+    cmd : async (interaction) => {
+        await interaction.reply('Pong!')
+        logger.info(\`The ping command was used. The bot "\${interaction.client.user.username}" replied to the user "\${interaction.user.tag}".\`)
+    }
+
+}
+    `
+    console.log(code)
 };
