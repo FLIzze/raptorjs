79b72acbee84dd9a125e378bc6fca4ab54d02a34
Erwan SINCK <erwan.sinck12@gmail.com>
2025-07-02 15:54:13 +0200

    test

--- src/cli/commands/addCommand.js
+++ src/cli/commands/addCommand.js
@@ -1,8 +1,27 @@
+import { input } from "@inquirer/prompts";
+
 export const addCommandFunc = async () => {
-    const commandName = "test"
-    const description = "test description"
+    const commandName = await input({
+        message: 'What is your command name ?',
+        validate: (value) => {
+            if (!value || value.trim() === "") return "Command name is required";
+            if (/[/\\?%*:|"<>]/.test(value)) return "Command name contains invalid characters";
+            if (existsSync(`./commands/${value}.js`) || existsSync(`./commands/${value}.ts`)) return "This Command name is already taken";
+            return true;
+        }
+    })
+    const description = await input({
+        message: 'What is your command description ?',
+        validate: (value) => {
+            if (!value || value.trim() === "") return "Command description is required";
+            return true;
+        }
+    })
     
     const code = `\
+import { Logger } from "raptorjs-discord"
+const logger = new Logger()
+
 export const PingCommand = {
 
     name:"${commandName}",
@@ -16,5 +35,7 @@ export const PingCommand = {
 
 }
     `
+
     console.log(code)
+    
 };
