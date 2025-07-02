68b67b12d6e8cd90d625eb910f39150f7ffdfdc1
Erwan SINCK <erwan.sinck12@gmail.com>
2025-07-02 15:57:30 +0200

    test

--- src/cli/commands/addCommand.js
+++ src/cli/commands/addCommand.js
@@ -2,24 +2,25 @@ import { input } from "@inquirer/prompts";
 import { existsSync } from "fs";
 
 export const addCommandFunc = async () => {
-    const commandName = await input({
-        message: 'What is your command name ?',
-        validate: (value) => {
-            if (!value || value.trim() === "") return "Command name is required";
-            if (/[/\\?%*:|"<>]/.test(value)) return "Command name contains invalid characters";
-            if (existsSync(`./commands/${value}.js`) || existsSync(`./commands/${value}.ts`)) return "This Command name is already taken";
-            return true;
-        }
-    })
-    const description = await input({
-        message: 'What is your command description ?',
-        validate: (value) => {
-            if (!value || value.trim() === "") return "Command description is required";
-            return true;
-        }
-    })
-    
-    const code = `\
+    try {
+        const commandName = await input({
+            message: 'What is your command name ?',
+            validate: (value) => {
+                if (!value || value.trim() === "") return "Command name is required";
+                if (/[/\\?%*:|"<>]/.test(value)) return "Command name contains invalid characters";
+                if (existsSync(`./src/commands/${value}.js`) || existsSync(`./src/commands/${value}.ts`)) return "This Command name is already taken";
+                return true;
+            }
+        })
+        const description = await input({
+            message: 'What is your command description ?',
+            validate: (value) => {
+                if (!value || value.trim() === "") return "Command description is required";
+                return true;
+            }
+        })
+        
+        const code = `\
 import { Logger } from "raptorjs-discord"
 const logger = new Logger()
 
@@ -34,9 +35,15 @@ export const PingCommand = {
         logger.info(\`The ping command was used. The bot "\${interaction.client.user.username}" replied to the user "\${interaction.user.tag}".\`)
     }
 
-}
-    `
-
-    console.log(code)
+}`
+        console.log(code)
+    } catch (err) {
+        if (err instanceof ExitPromptError) {
+            exit(1)
+        } else {
+            console.error("Unexpected error:", err);
+            exit(1);
+        }
+    }
     
 };
